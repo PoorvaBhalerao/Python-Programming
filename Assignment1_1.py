babe217b8788ee5561a5f96dd6404bcec75a3b4f
PoorvaBhalerao <poorvapatankar55@gmail.com>
2024-04-22 17:46:31 +0530

    assignment 1_1

---
+++ Assignment1_1.py
@@ -0,0 +1,11 @@
+# Write a program which contains one function named as Fun(). Tzhat function should display
+# "Hello from Fun" on console
+
+def Fun():
+    print("Hello From Fun")
+
+def main():
+    Fun()
+
+if __name__ == "__main__":
+    main()
