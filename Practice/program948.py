ef79ec77692bcc4df308d0840af9d952ef03093a
PoorvaBhalerao <151721149+PoorvaBhalerao@users.noreply.github.com>
2026-03-25 11:07:57 +0530

    Add files via upload

---
+++ Practice/program948.py
@@ -0,0 +1,23 @@
+#Accept number from user and display sum of factors of that number
+
+def SumFactors(No):
+    
+    iSum = 0
+
+    for i in range(1, int((No/2)+1)):
+        if(No % i == 0):
+            iSum = iSum + i
+
+    return iSum
+
+def main():
+    Value = 0
+
+    print("Enter number : ")
+    Value = int(input())
+
+    Ret = SumFactors(Value)
+    
+    print("Summation of factors : ",Ret)
+
+main()
---
+++ Practice/program949.py
@@ -0,0 +1,26 @@
+#Accept Number from user and check whether it is perfect or not
+
+def CheckPerfect(No):
+    
+    iSum = 0
+
+    for i in range(1, int((No/2)+1)):
+        if(No % i == 0):
+            iSum = iSum + i
+
+    return (iSum == No)
+
+def main():
+    Value = 0
+
+    print("Enter number : ")
+    Value = int(input())
+
+    Ret = CheckPerfect(Value)
+    
+    if(Ret == True):
+        print("It is perfect number")
+    else:
+        print("It is not a perfect number")
+
+main()
