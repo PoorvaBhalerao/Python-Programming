16b84bc1cdb0daf5acd491ca9ee9ed21cf871d3b
PoorvaBhalerao <151721149+PoorvaBhalerao@users.noreply.github.com>
2026-04-02 13:26:41 +0530

    Add files via upload
    Python Programming Practice

---
+++ Practice/program968.py
@@ -0,0 +1,21 @@
+
+
+#Accept string and count number of capital characters from string
+def CountCapital(Brr):
+    iCount = 0
+
+    for i in range(len(Brr)):
+        if(Brr[i] >= 65 and Brr[i] <= 90):  # Issue
+            iCount = iCount + 1
+
+    return iCount
+
+def main():
+    print("Enter string : ")
+    Arr = input() 
+
+    Ret = CountCapital(Arr)
+
+    print("Number of captial characters are : ",Ret)
+    
+main()
